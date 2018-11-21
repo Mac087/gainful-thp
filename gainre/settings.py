17a15fd6c92f202d3856d0f12e5ad943b31ee483
Andrew MacDonald <mac087@gmail.com>
2018-11-20 20:31:51 -0800

    host added

--- gainre/settings.py
+++ gainre/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'lgghc%cm(2kzsz0v&=eje2!vnm7^1vsl!$3d7l_ff3=@!*46k0'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['gain-thp-dev.us-west-2.elasticbeanstalk.com']
 
 
 # Application definition
