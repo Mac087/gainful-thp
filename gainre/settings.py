1c6c07d9a4b145978ca4fe3bc922ecf81eb56667
Andrew MacDonald <mac087@gmail.com>
2018-11-20 19:57:07 -0800

    www added

--- gainre/settings.py
+++ gainre/settings.py
@@ -75,15 +75,28 @@ WSGI_APPLICATION = 'gainre.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'gainre',
-        'USER': 'postgres',
-        'PASSWORD': 'student123',
-        'HOST': 'localhost'
+if 'RDS_DB_NAME' in os.environ:
+     DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql_psycopg2',
+            'NAME': os.environ['RDS_DB_NAME'],
+            'USER': os.environ['RDS_USERNAME'],
+            'PASSWORD': os.environ['RDS_PASSWORD'],
+            'HOST': os.environ['RDS_HOSTNAME'],
+            'PORT': os.environ['RDS_PORT'],
+        }
     }
-}
+
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql',
+            'NAME': 'gainre',
+            'USER': 'postgres',
+            'PASSWORD': 'student123',
+            'HOST': 'localhost'
+        }
+    }
 
 
 # Password validation
@@ -122,7 +135,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, '..', 'www', 'static')
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'gainre/static')
